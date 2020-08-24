15607e01c6f0f8cd8b81b738273e33ae7a25d9d0
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-25 01:21:56 +0530

    Added database

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:note_pad/model/notes.dart';
 import 'package:note_pad/screens/notepad.dart';
+import 'package:note_pad/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'notepad.dart';
 
 class HomeScreenLayout extends StatefulWidget {
   @override
@@ -7,8 +12,17 @@ class HomeScreenLayout extends StatefulWidget {
 }
 
 class _HomeScreenLayoutState extends State<HomeScreenLayout> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Notes> noteList;
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
+    if (noteList == null) {
+      noteList = List<Notes>();
+      updateListView();
+    }
+
     return Scaffold(
       body: Container(
         child: CustomScrollView(
@@ -99,7 +113,7 @@ class _HomeScreenLayoutState extends State<HomeScreenLayout> {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => NotepadScreen(),
+              builder: (context) => NotepadScreen(Notes("", " ", 2)),
             ),
           );
         },
@@ -110,5 +124,112 @@ class _HomeScreenLayoutState extends State<HomeScreenLayout> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  ListView getNoteListView() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor:
+                  getPriorityColor(this.noteList[position].priority),
+              child: getPriorityIcon(this.noteList[position].priority),
+            ),
+            title: Text(
+              this.noteList[position].title,
+            ),
+            subtitle: Text(this.noteList[position].date),
+            trailing: GestureDetector(
+              child: Icon(
+                Icons.delete,
+                color: Colors.grey,
+              ),
+              onTap: () {
+                _delete(context, noteList[position]);
+              },
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+              // navigateToDetail(this.noteList[position], 'Edit Note');
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  // Returns the priority color
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  // Returns the priority icon
+  Icon getPriorityIcon(int priority) {
+    switch (priority) {
+      case 1:
+        return Icon(Icons.play_arrow);
+        break;
+      case 2:
+        return Icon(Icons.keyboard_arrow_right);
+        break;
+
+      default:
+        return Icon(Icons.keyboard_arrow_right);
+    }
+  }
+
+  void _delete(BuildContext context, Notes note) async {
+    int result = await databaseHelper.deleteNote(note.id);
+    if (result != 0) {
+      _showSnackBar(context, 'Note Deleted Successfully');
+      updateListView();
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(content: Text(message));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  // void navigateToDetail() {
+  //   bool result = Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) {
+  //         NotepadScreen();
+  //       },
+  //     ),
+  //   );
+
+  //   if (result == true) {
+  //     updateListView();
+  //   }
+  // }
+
+  void updateListView() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Notes>> noteListFuture = databaseHelper.getNoteList();
+      noteListFuture.then((noteList) {
+        setState(() {
+          this.noteList = noteList;
+          this.count = noteList.length;
+        });
+      });
+    });
   }
 }
--- lib/screens/notepad.dart
+++ lib/screens/notepad.dart
@@ -1,85 +1,167 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+import 'package:note_pad/model/notes.dart';
+import 'package:note_pad/utils/database_helper.dart';
 
 class NotepadScreen extends StatefulWidget {
+  final Notes note;
+
+  NotepadScreen(this.note);
   @override
-  _NotepadScreenState createState() => _NotepadScreenState();
+  State<StatefulWidget> createState() {
+    return _NotepadScreenState(this.note);
+  }
 }
 
 class _NotepadScreenState extends State<NotepadScreen> {
+  DatabaseHelper helper = DatabaseHelper();
+
+  Notes note;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  _NotepadScreenState(this.note);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
-        title: Container(
-          alignment: Alignment.centerRight,
-          child: Text(
-            "Save",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        leading: Icon(
-          Icons.text_fields,
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.attach_file,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-          PopupMenuButton(
-            itemBuilder: (BuildContext context) {
-              return null;
+    titleController.text = note.title;
+    descriptionController.text = note.description;
+    return WillPopScope(
+      onWillPop: () {
+        // Write some code to control things, when user press Back navigation button in device navigationBar
+        moveToLastScreen();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
+          title: GestureDetector(
+            onTap: () {
+              setState(() {
+                debugPrint("you clicked me");
+                _save();
+              });
             },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding:
-            const EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 0),
-        child: Column(
-          children: [
-            TextField(
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                hintText: "Title",
-                border: InputBorder.none,
-              ),
-              autofocus: true,
-              keyboardType: TextInputType.multiline,
-            ),
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Notes",
-                  border: InputBorder.none,
+            child: Container(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "Save",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
-                scrollPadding: EdgeInsets.all(20.0),
-                autofocus: true,
-                maxLines: 999999,
-                keyboardType: TextInputType.multiline,
               ),
+            ),
+          ),
+          leading: Icon(
+            Icons.text_fields,
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.attach_file,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) {
+                return null;
+              },
             ),
           ],
         ),
-      ),
-      resizeToAvoidBottomPadding: true,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Container(
-          color: Colors.lightBlueAccent,
-          height: 60,
+        body: Padding(
+          padding:
+              const EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 0),
+          child: Column(
+            children: [
+              TextField(
+                controller: titleController,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  border: InputBorder.none,
+                ),
+                autofocus: true,
+                keyboardType: TextInputType.multiline,
+              ),
+              Expanded(
+                child: TextField(
+                  controller: descriptionController,
+                  onChanged: (value) {
+                    debugPrint("something is changed");
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Notes",
+                    border: InputBorder.none,
+                  ),
+                  scrollPadding: EdgeInsets.all(20.0),
+                  autofocus: true,
+                  maxLines: 999999,
+                  keyboardType: TextInputType.multiline,
+                ),
+              ),
+            ],
+          ),
+        ),
+        resizeToAvoidBottomPadding: true,
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Container(
+            color: Colors.lightBlueAccent,
+            height: 60,
+          ),
         ),
       ),
     );
+  }
+
+  void moveToLastScreen() {
+    Navigator.pop(context, true);
+  }
+
+  // Update the title of Note object
+  void updateTitle() {
+    note.title = titleController.text;
+  }
+
+  // Update the description of Note object
+  void updateDescription() {
+    note.description = descriptionController.text;
+  }
+
+  // Save data to database
+  void _save() async {
+    moveToLastScreen();
+
+    note.date = DateFormat.yMMMd().format(DateTime.now());
+    int result;
+    if (note.id != null) {
+      // Case 1: Update operation
+      result = await helper.updateNote(note);
+    } else {
+      // Case 2: Insert Operation
+      result = await helper.insertNote(note);
+    }
+
+    if (result != 0) {
+      // Success
+      _showAlertDialog('Status', 'Note Saved Successfully');
+    } else {
+      // Failure
+      _showAlertDialog('Status', 'Problem Saving Note');
+    }
+  }
+
+  void _showAlertDialog(String title, String message) {
+    AlertDialog alertDialog = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+    );
+    showDialog(context: context, builder: (_) => alertDialog);
   }
 }
